c81422ccf9165b3599fb4a4086f6be9f3f5ad4a3
Alex Suraci <suraci.alex@gmail.com>
2022-01-15 14:27:45 -0500

    fix deadlocks once and for all

--- chan.go
+++ chan.go
@@ -6,8 +6,20 @@ import (
 	"github.com/vito/progrock/graph"
 )
 
+// PipeBuffer is the number of writes to allow before a read must occur.
+//
+// This value is arbitrary. I don't want to leak this implementation detail to
+// the API because it feels like a bit of a smell that this is even necessary,
+// and perhaps the rest of the API can be refactored instead.
+//
+// The main idea is to be nonzero to prevent any weird deadlocks that occur
+// around initialization/exit time due to an error in an inopportune moment.
+// I'm tired of chasing down deadlocks and this seems like a low risk band-aid.
+// Refactors welcome.
+const PipeBuffer = 100
+
 func Pipe() (ChanReader, Writer) {
-	ch := make(chan *graph.SolveStatus)
+	ch := make(chan *graph.SolveStatus, PipeBuffer)
 	return ch, &ChanWriter{ch: ch}
 }
 
