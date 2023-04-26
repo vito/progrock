198ee3cc6e880e3028c0836079c054884c2f2564
Alex Suraci <suraci.alex@gmail.com>
2023-04-26 00:14:42 -0400

    add groups

--- recorder.go
+++ recorder.go
@@ -1,11 +1,11 @@
 package progrock
 
 import (
-	"io"
-	"sync"
+	"fmt"
 
 	"github.com/jonboulle/clockwork"
 	"google.golang.org/protobuf/proto"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // Clock is used to determine the current time.
@@ -13,37 +13,64 @@ var Clock = clockwork.NewRealClock()
 
 type Recorder struct {
 	w Writer
-
-	displaying *sync.WaitGroup
+	g *Group
 }
 
-func NewRecorder(w Writer) *Recorder {
-	return &Recorder{
+// RootGroup is the name of the toplevel group, which is blank.
+const RootGroup = ""
+
+// NewRecorder creates a new Recorder, which writes to the given Writer.
+//
+// It also initializes the "root" group with the associated labels, which
+// involves sending a progress update for the group.
+func NewRecorder(w Writer, labels ...*Label) *Recorder {
+	return (&Recorder{
 		w: w,
-
-		displaying: &sync.WaitGroup{},
-	}
+	}).Group(RootGroup, labels...)
 }
 
-type LogSink struct {
-	Stdout io.Writer
-	Stderr io.Writer
-}
-
+// Record sends a deep-copy of the status update to the Writer.
 func (recorder *Recorder) Record(status *StatusUpdate) {
-	for i, v := range status.Vertexes {
-		cp := proto.Clone(v)
-		status.Vertexes[i] = cp.(*Vertex)
+	// perform a deep-copy so buffered writes don't get mutated, similar to
+	// copying in Write([]byte) when []byte comes from sync.Pool
+	recorder.w.WriteStatus(proto.Clone(status).(*StatusUpdate))
+}
+
+func (recorder *Recorder) Group(name string, labels ...*Label) *Recorder {
+	now := Clock.Now()
+
+	id := fmt.Sprintf("%s@%d", name, now.UnixNano())
+
+	g := &Group{
+		Id:      id,
+		Name:    name,
+		Labels:  labels,
+		Started: timestamppb.New(now),
 	}
 
-	for i, t := range status.Tasks {
-		cp := proto.Clone(t)
-		status.Tasks[i] = cp.(*VertexTask)
+	if recorder.g != nil {
+		g.Parent = &recorder.g.Id
 	}
 
-	recorder.w.WriteStatus(status)
+	recorder.sync()
+
+	return &Recorder{
+		w: recorder.w,
+		g: g,
+	}
+}
+
+func (recorder *Recorder) Complete() {
+	recorder.g.Completed = timestamppb.New(Clock.Now())
+	recorder.sync()
 }
 
 func (recorder *Recorder) Close() error {
 	return recorder.w.Close()
+}
+
+func (recorder *Recorder) sync() {
+	recorder.Record(&StatusUpdate{
+		Groups: []*Group{recorder.g},
+	})
 }
