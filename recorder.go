a440e24b81a468b4649f9634063b746232fe00ae
Alex Suraci <suraci.alex@gmail.com>
2023-05-03 23:06:09 -0400

    remove vertexGroups (no longer used)

--- recorder.go
+++ recorder.go
@@ -5,7 +5,6 @@ import (
 	"sync"
 
 	"github.com/jonboulle/clockwork"
-	"github.com/opencontainers/go-digest"
 	"google.golang.org/protobuf/proto"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -110,51 +109,4 @@ func (recorder *Recorder) sync() {
 	recorder.Record(&StatusUpdate{
 		Groups: []*Group{recorder.Group},
 	})
-}
-
-// vertexGroups tracks the union of all groups seen for a vertex digest.
-type vertexGroups struct {
-	vg map[digest.Digest]map[string]struct{}
-	l  sync.Mutex
-}
-
-func newVertexGroups() *vertexGroups {
-	return &vertexGroups{
-		vg: map[digest.Digest]map[string]struct{}{},
-	}
-}
-
-// Add adds the given groups to the vertex digest.
-func (g *vertexGroups) Add(vertex digest.Digest, groups ...string) {
-	g.l.Lock()
-	defer g.l.Unlock()
-
-	for _, group := range groups {
-		if _, ok := g.vg[vertex]; !ok {
-			g.vg[vertex] = map[string]struct{}{}
-		}
-
-		g.vg[vertex][group] = struct{}{}
-	}
-}
-
-// Groups returns the union of all groups seen for a vertex digest. If no
-// groups are found, the defaultGroup is returned.
-func (g *vertexGroups) Groups(vertex digest.Digest, defaultGroup string) []string {
-	g.l.Lock()
-	defer g.l.Unlock()
-
-	var out []string
-	groups, ok := g.vg[vertex]
-	if ok {
-		for group := range groups {
-			out = append(out, group)
-		}
-	}
-
-	if len(out) == 0 {
-		out = append(out, defaultGroup)
-	}
-
-	return out
 }
