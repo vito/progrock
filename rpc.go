4c3a20e1ec20b1fecf02943bae9ef049466a73d4
Alex Suraci <suraci.alex@gmail.com>
2023-07-06 17:06:58 -0400

    expose constructors for RPC reader/writer, add godoc

--- rpc.go
+++ rpc.go
@@ -11,11 +11,22 @@ import (
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
-type RPCWriter struct {
-	Conn    *grpc.ClientConn
-	Updates ProgressService_WriteUpdatesClient
+// ServeRPC serves a ProgressService over the given listener.
+func ServeRPC(l net.Listener, w Writer) (Writer, error) {
+	recv := NewRPCReceiver(w)
+
+	srv := grpc.NewServer()
+	RegisterProgressServiceServer(srv, recv)
+
+	go srv.Serve(l)
+
+	return WaitWriter{
+		Writer: w,
+		srv:    srv,
+	}, nil
 }
 
+// DialRPC dials a ProgressService at the given target.
 func DialRPC(ctx context.Context, target string) (Writer, error) {
 	conn, err := grpc.DialContext(ctx, target, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -29,27 +40,47 @@ func DialRPC(ctx context.Context, target string) (Writer, error) {
 		return nil, err
 	}
 
+	return NewRPCWriter(conn, updates), nil
+}
+
+// RPCWriter is a Writer that writes to a ProgressService.
+type RPCWriter struct {
+	Conn    *grpc.ClientConn
+	Updates ProgressService_WriteUpdatesClient
+}
+
+// NewRPCWriter returns a new RPCWriter.
+func NewRPCWriter(conn *grpc.ClientConn, updates ProgressService_WriteUpdatesClient) *RPCWriter {
 	return &RPCWriter{
 		Conn:    conn,
 		Updates: updates,
-	}, nil
+	}
 }
 
+// WriteStatus implements Writer.
 func (w *RPCWriter) WriteStatus(status *StatusUpdate) error {
 	return w.Updates.Send(status)
 }
 
+// Close closes the underlying RPC connection.
 func (w *RPCWriter) Close() error {
 	_, err := w.Updates.CloseAndRecv()
 	return err
 }
 
+// RPCReceiver is a ProgressServiceServer that writes to a Writer.
 type RPCReceiver struct {
-	W Writer
+	w Writer
 
 	UnimplementedProgressServiceServer
 }
 
+// NewRPCReceiver returns a new RPCReceiver.
+func NewRPCReceiver(w Writer) *RPCReceiver {
+	return &RPCReceiver{w: w}
+}
+
+// WriteUpdates implements ProgressServiceServer.
 func (recv *RPCReceiver) WriteUpdates(srv ProgressService_WriteUpdatesServer) error {
 	for {
 		update, err := srv.Recv()
@@ -59,34 +90,21 @@ func (recv *RPCReceiver) WriteUpdates(srv ProgressService_WriteUpdatesServer) er
 			}
 			return err
 		}
-		if err := recv.W.WriteStatus(update); err != nil {
+		if err := recv.w.WriteStatus(update); err != nil {
 			return err
 		}
 	}
 }
 
-func ServeRPC(l net.Listener, w Writer) (Writer, error) {
-	recv := &RPCReceiver{
-		W: w,
-	}
-
-	srv := grpc.NewServer()
-	RegisterProgressServiceServer(srv, recv)
-
-	go srv.Serve(l)
-
-	return WaitWriter{
-		Writer: w,
-		srv:    srv,
-	}, nil
-}
-
+// WaitWriter is a Writer that waits for the RPC server to stop before closing
+// the underlying Writer.
 type WaitWriter struct {
 	Writer
 
 	srv *grpc.Server
 }
 
+// Close waits for the RPC server to stop and closes the underlying Writer.
 func (ww WaitWriter) Close() error {
 	ww.srv.GracefulStop()
 	return ww.Writer.Close()
