4246ea1cfc6d5ed795ebcc67e5c71b52ab92daa7
Alex Suraci <suraci.alex@gmail.com>
2023-07-30 16:19:04 -0400

    NewLabel -> Labelf
    more convenient in practice

--- label.go
+++ label.go
@@ -2,13 +2,19 @@ package progrock
 
 import "fmt"
 
-// NewLabel is a convenience function for creating a Label, primarily for use
-// as a MessageOpt.
-func NewLabel(name string, valueAny any) *Label {
-	value, ok := valueAny.(string)
-	if !ok {
-		value = fmt.Sprintf("%s", valueAny)
+// Labelf is a convenience function for creating a Label from a format string
+// and values.
+//
+// If no values are given, format is used verbatim, so Labelf also doubles as a
+// plain string value Label constructor.
+func Labelf(name string, format string, vals ...any) *Label {
+	var value string
+	if len(vals) > 0 {
+		value = format
+	} else {
+		value = fmt.Sprintf(format, vals...)
 	}
+
 	return &Label{
 		Name:  name,
 		Value: value,
